3604bbfe0d9ac35afd0e7041b9124cfa3b04987a
yelinko20 <119092367+yelinko20@users.noreply.github.com>
2023-05-01 10:34:47 +0630

    Update portfolio.tsx

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -32,25 +32,35 @@ export default function Portfolio() {
             business outcomes.
           </p>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
-            {projects.map(project => (
-              <div key={project.id} className="bg-Blur p-4 rounded-lg">
-                <div className="w-full h-3/4 relative group">
-                  <img src={project.img} alt="" className="w-full h-full object-cover"/>
+            {projects.map((project) => (
+              <div
+                key={project.id}
+                className="bg-Blur p-4  rounded-lg"
+              >
+                <div className="w-full  relative group">
+                  <img
+                    src={project.img}
+                    alt=""
+                    className="w-full h-full object-cover"
+                  />
                   <div className="absolute w-full h-full bg-Orange top-0 left-0 z-10 flex items-center justify-center gap-4 scale-0 group-hover:scale-100 transition-all duration-300">
                     <Link href="">
-                      <AiFillGithub className="text-3xl hover:scale-110 transition-all duration-300"/>
+                      <AiFillGithub className="text-3xl hover:scale-110 transition-all duration-300" />
                     </Link>
                     <Link href="">
-                      <AiFillEye className="text-3xl hover:scale-110 transition-all duration-300"/>
+                      <AiFillEye className="text-3xl hover:scale-110 transition-all duration-300" />
                     </Link>
                   </div>
                 </div>
-                <div className="text-White mt-2 text-start">
-                  {project.name}
-                </div>
-                <div className="flex mt-2 flex wrap">
-                  {project.tools.map(tool => (
-                    <span key={tool} className="bg-Blur mr-2 rounded-full py-2 px-4 text-white text-sm">{tool}</span>
+                <div className="text-White mt-2 text-start">{project.name}</div>
+                <div className="flex mt-2 flex-wrap gap-2">
+                  {project.tools.map((tool) => (
+                    <span
+                      key={tool}
+                      className="bg-Blur mr-2 rounded-full py-2 px-4 text-white text-sm"
+                    >
+                      {tool}
+                    </span>
                   ))}
                 </div>
               </div>
